1d87d5eab4c019b2d4e988ef626a46ef35805118
Times_name <342730241@qq.com>
2019-12-04 18:02:44 +0800

    添加1

---
+++ pages/personal/wallet/theDetail.vue
@@ -0,0 +1,113 @@
+<template>
+	<view class="bgColor he">
+		<view class="headerMoney">
+			<text class="text">总收入积分</text>
+			<text class="money">360</text>
+			<view class="date">
+				<image src="../../../static/image/dataTime.png" mode="" class="dataTim"></image>
+				<text>2019 . 08</text>
+			</view>
+		</view>
+		<view class="list-money list-price">
+			<view class="list">
+				<view class="textarr">
+					<text>图文咨询</text>
+					<text class="date">2019-02-11</text>
+				</view>
+				<view class="price">
+					<text>30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="gd"></image>
+				</view>
+			</view>
+			<view class="list">
+				<view class="textarr">
+					<text>图文咨询</text>
+					<text class="date">2019-02-08</text>
+				</view>
+				<view class="price">
+					<text>30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="gd"></image>
+				</view>
+			</view>
+			<view class="list">
+				<view class="textarr">
+					<text>视频咨询</text>
+					<text class="date">2019-02-11</text>
+				</view>
+				<view class="price">
+					<text>30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="gd"></image>
+				</view>
+			</view>
+		</view>
+		<view class="textNext">
+			<!-- 只能用到微信小程序中 -->
+			<!--#ifdef MP-WEIXIN-->
+			<button type="button" class="loguin">申请提现</button>
+			 <!-- #endif -->
+			 <!--除微信小程序平台其它平台都可以用 -->
+			 <!--#ifndef MP-WEIXIN-->
+			 <button type="primary">申请提现</button>
+			 <!-- #endif -->
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.headerMoney{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #FFFFFF;
+	padding: 35upx 0;
+	margin-top: 30upx;
+}
+.headerMoney .text{
+	font-size: 30upx;
+	color: #999999;
+}
+.headerMoney .money{
+	font-size: 80upx;
+	color: #f98e47;
+}
+.headerMoney .date .dataTim{
+	width: 33upx;
+	height: 32upx;
+	margin-right: 20upx;
+}
+.headerMoney .date{
+	color: #333333;
+	font-size: 36upx;
+}
+.list-price .textarr text:last-child{
+	font-size: 24upx;
+	color: #cccccc;
+}
+.list-price .textarr text:first-child{
+	font-size: 28upx;
+	color: #4a4a4a;
+	margin-right: 30upx;
+}
+.list-price .price text{
+	font-size: 28upx;
+	color: #4a4a4a;
+	margin-right: 20upx;
+}
+.textNext{
+	width: 90%;
+	margin: 100upx auto 0;
+}
+</style>
---
+++ pages/personal/wallet/withdrawal.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="bgColor he">
+		<view class="listPers withdra">
+			<view class="feiyong-arrow" @tap="wallet">
+				<view class="feiyong">
+					<image src="../../../static/image/audit.png" mode=""></image>
+					<view class="time-describe">
+						<text class="time">2018-08-30  17:20</text>
+						<text class="describe">审核描述：待审核</text>
+					</view>
+				</view>
+				<view class="rightM">
+					<text>－30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="arrow"></image>
+				</view>
+			</view>
+			<view class="feiyong-arrow" @tap="wallet">
+				<view class="feiyong">
+					<image src="../../../static/image/errors.png" mode=""></image>
+					<view class="time-describe">
+						<text class="time">2018-08-30  17:20</text>
+						<text class="describe">审核描述：审核失败</text>
+					</view>
+				</view>
+				<view class="rightM">
+					<text>－30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="arrow"></image>
+				</view>
+			</view>
+			<view class="feiyong-arrow" @tap="wallet">
+				<view class="feiyong">
+					<image src="../../../static/image/transfer.png" mode=""></image>
+					<view class="time-describe">
+						<text class="time">2018-08-30  17:20</text>
+						<text class="describe">审核描述：待转账</text>
+					</view>
+				</view>
+				<view class="rightM">
+					<text>－30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="arrow"></image>
+				</view>
+			</view>
+			<view class="feiyong-arrow" @tap="wallet">
+				<view class="feiyong">
+					<image src="../../../static/image/successs.png" mode=""></image>
+					<view class="time-describe">
+						<text class="time">2018-08-30  17:20</text>
+						<text class="describe">审核描述：已提现</text>
+					</view>
+				</view>
+				<view class="rightM">
+					<text>－30.00</text>
+					<image src="../../../static/image/arrow.png" mode="" class="arrow"></image>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.withdra{
+	margin-top: 30upx;
+	
+}
+.withdra .feiyong-arrow{
+	height: 110upx;
+}
+.withdra .feiyong-arrow:last-child{
+	border-bottom: none;
+}
+.withdra .time-describe {
+	display: flex;
+	flex-direction: column;
+}
+.withdra .time-describe .time{
+	font-size: 32upx;
+	color: #333333;
+}
+.withdra .time-describe .describe{
+	font-size: 26upx;
+	color: #cccccc;
+}
+.withdra .rightM text{
+	margin-right: 20upx;
+	color: #fc2702;
+	font-size: 28upx;
+}
+</style>
